c50c4a95a782251e0d689badb07b352097c4d020
Haytam Mintaki <129561301+haytammintakii@users.noreply.github.com>
2023-04-01 16:24:22 +0800

    Add files via upload

---
+++ Dice error.py
@@ -0,0 +1,15 @@
+
+import numpy as np
+import cv2
+from keras import backend as K
+gt=cv2.imread("your ground truth",0)
+seg=cv2.imread("predicted image",0)
+a = np.where(gt!=255, 1, gt)
+a = np.where(gt==255, 0, a)
+gt=a
+b = np.where(seg!=255, 1, seg)
+b = np.where(seg==255, 0, b)
+seg=b
+k=1
+dice = np.sum(seg[gt==k]) *2/ (np.sum(seg) + np.sum(gt))
+print(1-dice)
